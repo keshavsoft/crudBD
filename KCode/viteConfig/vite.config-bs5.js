58d93c7a9d07bbef403e7889c3a0d1ca519e3aa6
keshavsoft <nknnkumar@yahoo.com>
2024-07-18 20:30:19 +0530

    dashboard with bs5

--- KCode/viteConfig/vite.config-bs5.js
+++ KCode/viteConfig/vite.config-bs5.js
@@ -1,20 +1,67 @@
-import { resolve } from 'path';
+import { defineConfig, normalizePath, build } from 'vite';
 import handlebars from 'vite-plugin-handlebars';
-import ConfigJson from '../../bin/Config.json' with {type: 'json'};
+// import ConfigJson from '../../bin/Config.json' with {type: 'json'};
 
-const commonSrcFolder = "../../src/Index/FrontEnd";
-const commonDistFolder = "../../../public";
+import path, { resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+import { StartFunc as StartFuncGetFiles } from "./getFiles.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const commonSrcFolder = "src/Index/FrontEnd";
+const commonDistFolder = "public";
+
+let files = StartFuncGetFiles({ inSrcPath: commonSrcFolder });
+console.log("files : ", files);
+
+const pageData = {
+    '/index.html': {
+        title: 'Main Page',
+    }
+};
+
+build({
+    configFile: false,
+    build: {
+        emptyOutDir: false,
+        outDir: resolve(__dirname, commonDistFolder),
+        lib: {
+            name: 'app',
+            formats: ['umd'],
+            fileName: 'app',
+            entry: `${commonSrcFolder}/js/app.js`,
+        },
+        rollupOptions: {
+            output: {
+                entryFileNames: '[name].js'
+            }
+        }
+    },
+});
 
 export default {
-    root: resolve(__dirname, commonSrcFolder),
-    plugins: [handlebars({
-        context: {
-            title: 'KeshavSoft',
-            DataPk: ConfigJson.jsonConfig.DataPk
-        },
-    })],
+    publicDir: 'static',
+    base: './',
+    root: resolve(__dirname, `../../${commonSrcFolder}`),
+    plugins: [
+        handlebars({
+            context(pagePath) {
+                return files[pagePath];
+            },
+        }),
+    ],
     build: {
-        outDir: commonDistFolder
+        emptyOutDir: false,
+        manifest: true,
+        target: "chrome58",
+        outDir: resolve(__dirname, `../../${commonDistFolder}`),
+        rollupOptions: {
+            input: files,
+            output: {
+            }
+        },
     },
     server: {
         port: 8080
--- src/Index/FrontEnd/js/main.js
+++ src/Index/FrontEnd/js/main.js
@@ -2,7 +2,7 @@
 import '../scss/styles.scss'
 
 // Import only the Bootstrap components we need
-import { Popover } from 'bootstrap';
+import * as bootstrap from 'bootstrap'
 
 // Create an example popover
 document.querySelectorAll('[data-bs-toggle="popover"]')
