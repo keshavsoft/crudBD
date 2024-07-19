dba146b00c1824fd31511b2c4266bb328f634e0a
keshavsoft <nknnkumar@yahoo.com>
2024-07-19 21:14:06 +0530

    fixed default values for json get

--- src/BackEnd/ksSample/kLowDb/GetTableSchema/ColumnsAsObject.js
+++ src/BackEnd/ksSample/kLowDb/GetTableSchema/ColumnsAsObject.js
@@ -1,5 +1,6 @@
 import Configjson from '../../../Config.json' assert { type: 'json' };
 import tableNameJson from '../../tableName.json' assert { type: 'json' };
+import { StartFunc as PullData } from "../PullData/EntryFile.js";
 
 let StartFunc = () => {
     let LocalReturnData = { KTF: false, JSONFolderPath: "", CreatedLog: {} };
@@ -18,18 +19,40 @@ let StartFunc = () => {
 
     let LocalColumnsOnly = Object.keys(LocalFindColumns.fileData);
     let LocalReturnObject = convertArrayToObject(LocalColumnsOnly);
-    
+
+    LocalFuncInsertDefaultValues({ inObject: LocalReturnObject, inTableSchema: LocalFindColumns.fileData })
+
     return LocalReturnObject;
 };
 
-const convertArrayToObject = (array ) => {
+const LocalFuncInsertDefaultValues = ({ inObject, inTableSchema }) => {
+    for (const [key, value] of Object.entries(inTableSchema)) {
+        if (value.presentDate) {
+            inObject[key] = new Date().toISOString().split('T')[0];
+        };
+
+        if (value.max) {
+            let LocalData = PullData();
+
+            let LocalArrayPk = LocalData.inDb.data.map(element => element[key]);
+
+            let numberArray = LocalArrayPk.map(Number);
+
+            let MaxPk = (Math.max(...numberArray, 0) + 1);
+
+            inObject[key] = MaxPk;
+        };
+    };
+};
+
+const convertArrayToObject = (array) => {
     const initialValue = {};
     return array.reduce((obj, item) => {
-      return {
-        ...obj,
-        [item]: "",
-      };
+        return {
+            ...obj,
+            [item]: "",
+        };
     }, initialValue);
-  };
+};
 
 export { StartFunc };
