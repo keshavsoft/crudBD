5bea10297d2dd6da193e5a14464f59ebb2e06326
Srivasu215 <sri.vasu215@gmail.com>
2024-07-19 13:19:22 +0530

    SREENIVAS

--- src/BackEnd/ksSample/routes/postFuncs/EntryFile.js
+++ src/BackEnd/ksSample/routes/postFuncs/EntryFile.js
@@ -3,7 +3,7 @@ import express from 'express';
 var router = express.Router();
 
 import {
-    PostFunc, PostFromModalFunc,PostCustomPkFunc,
+    PostFunc, PostFromModalFunc, PostCustomPkFunc,
     PostUploadFunc, PostGetSelectColumnsFunc, PostUploadFromModalFunc,
     PostUploadImageFunc, PostFilterFunc, PostWithKeysCheckFunc, PostFuncGenUuId, PostWithCheckAndGenPkFunc, MultiInsertWithCheckFunc
 } from '../../controllers/postFuncs/EntryFile.js';
@@ -19,13 +19,15 @@ import { upload as uploadFromMulter } from '../../dals/postFuncs/UsingMulter.js'
 
 router.post('/BodyCheck', PostFuncmiddleware, PostFunc);
 router.post('/GetSelectColumns', PostGetSelectColumnsFunc);
+// Post - it's check foreign and unique check and it's collect Max pk and generate UuId And pk also
 router.post('/', PostFunc);
 router.post('/CustomPk', PostCustomPkFunc);
 router.post('/GenUuId', PostFuncGenUuId);
+// WithKeysCheck - it's check foreign and unique check and it's collect Max pk and generate UuId
 router.post('/WithKeysCheck', PostWithKeysCheckFunc);
+// WithCheckAndGenPk - it's check foreign and unique check and it's collect Max pk and generate pk
 router.post('/WithCheckAndGenPk', PostWithCheckAndGenPkFunc);
 router.post('/MultiInsertWithCheck', MultiInsertWithCheckMiddleware, MultiInsertWithCheckFunc);
-
 router.post('/Filter', PostFilterFunc);
 router.post('/FromModal', PostFromModalFunc);
 router.post('/Upload', PostFuncPostUploadFunc, PostUploadFunc);
