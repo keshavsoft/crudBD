b331feb772a9494c9eb3103a8b808c7575f1bd8f
keshavsoft <nknnkumar@yahoo.com>
2024-07-27 11:24:50 +0530

    cd wrote in batch file

--- FrontATSubTableSideBar.bat
+++ FrontATSubTableSideBar.bat
@@ -7,3 +7,4 @@ xcopy ".\KCode\TableSchema\%datapk%\*.json" ..\FrontEndByVite\TableSchema
 cd ..\FrontEndByVite
 call npm run buildATSubTableSideBar
 xcopy .\publicDir\binATSubTableSideBar ..\crudBD\public\binATSubTableSideBar /h /i /c /k /e /r /y
+cd ..\crudBD
