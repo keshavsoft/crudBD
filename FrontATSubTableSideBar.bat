54f7c45b66be1afe7c56c07570f6b9292f3b980b
keshavsoft <nknnkumar@yahoo.com>
2024-08-01 17:16:32 +0530

    started laundry

--- FrontATSubTableSideBar.bat
+++ FrontATSubTableSideBar.bat
@@ -3,6 +3,8 @@ set /p "datapk=Enter dataPk needed : "
 
 del /p ..\FrontEndByVite\ColumnSchema\*.json
 del /p ..\FrontEndByVite\TableSchema\*.json
+del /p ..\FrontEndByVite\src\AllTables\WithSSR\SubTableSideBar\SourceCode\HtmlFiles\*.html
+del /p ..\FrontEndByVite\src\AllTables\WithSSR\SubTableSideBar\SourceCode\Js\pages\*.*
 
 xcopy ".\KCode\DataSchema\%datapk%\*.json" ..\FrontEndByVite\ColumnSchema
 xcopy ".\KCode\TableSchema\%datapk%\*.json" ..\FrontEndByVite\TableSchema
