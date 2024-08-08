cbbcb292f6fce4ff27f046cdfb1d706de9651c4c
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2024-08-08 16:17:06 +0530

    commented login middleware

--- Login/routes.js
+++ Login/routes.js
@@ -4,9 +4,9 @@ var router = express.Router();
 
 import { router as routerFrombin } from './LoginBin/routes.js';
 import { router as routerLoginAdmin } from './LoginAdmin/routes.js';
-import { StartFunc as routerMiddleWaresLoginAdmin } from './MiddleWares/MiddleWares.LoginAdmin/EntryFile.js';
+// import { StartFunc as routerMiddleWaresLoginAdmin } from './MiddleWares/MiddleWares.LoginAdmin/EntryFile.js';
 
-router.use('/bin', routerMiddleWaresLoginAdmin, routerFrombin);
+router.use('/bin', routerFrombin);
 router.use('/LoginAdmin', routerLoginAdmin);
 
 export { router };
