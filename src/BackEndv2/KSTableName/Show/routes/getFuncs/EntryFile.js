050390461a51b36dcd247fd3e7377ebc3a0569db
keshavsoft <nknnkumar@yahoo.com>
2024-08-13 17:34:29 +0530

    started WithJoins in v2

--- src/BackEndv2/KSTableName/Show/routes/getFuncs/EntryFile.js
+++ src/BackEndv2/KSTableName/Show/routes/getFuncs/EntryFile.js
@@ -7,12 +7,12 @@ import {
     GetFromModalUuidFunc
 } from '../../controllers/getFuncs/EntryFile.js';
 
-
 router.get('/', GetFunc);
 router.get('/DataOnly', GetDataOnlyFunc);
 router.get('/Images', GetImagesFunc);
 router.get('/BodyCheck', GetBodyCheckFunc);
 router.get('/FromModal', GetFromModalFunc);
 router.get('/FromModalUuid', GetFromModalUuidFunc);
+router.get('/WithJoins', GetFromModalUuidFunc);
 
 export { router };
