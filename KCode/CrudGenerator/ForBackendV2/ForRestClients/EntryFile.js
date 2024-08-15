2e05a549c13d64ec02aab330ff8da19a0b14449b
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2024-08-16 02:23:37 +0530

    v2 send mail need to be tweaked for url

--- KCode/CrudGenerator/ForBackendV2/ForRestClients/EntryFile.js
+++ KCode/CrudGenerator/ForBackendV2/ForRestClients/EntryFile.js
@@ -1,7 +1,9 @@
 import { StartFunc as Show } from './Show/EntryFile.js';
+import { StartFunc as Create } from './Create/EntryFile.js';
 
 let StartFunc = ({ inTablesCollection, inTo, inFrom }) => {
     Show({ inTablesCollection, inTo, inFrom });
+    Create({ inTablesCollection, inTo, inFrom });
 };
 
 export { StartFunc };
