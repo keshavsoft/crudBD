f57ec9c5ab66861eed419cffd02111f5640204a5
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2024-08-20 22:24:43 +0530

    AllUniques working in common

--- Common/CommonBin/PrimaryTables/WithData/kLowDb/AllUniques.js
+++ Common/CommonBin/PrimaryTables/WithData/kLowDb/AllUniques.js
@@ -2,7 +2,27 @@ import fs from "fs";
 import ConfigJson from '../../../../../bin/Config.json' assert {type: 'json'};
 import path from "path";
 
+let CommonDataPath = `${ConfigJson.jsonConfig.DataPath}/${ConfigJson.jsonConfig.DataPk}`;
+
 let StartFunc = () => {
+    let LocalForeignTables = LocalFuncReturnForeignTables();
+
+    let LocalReturnData = { KTF: false }
+    LocalReturnData.JsonData = {};
+
+    let filenames = LocalFuncReturnFiles();
+
+    LocalReturnData.JsonData = LocalFuncLoopFiles({
+        inForeignTables: LocalForeignTables,
+        inFileNames: filenames
+    });
+
+    LocalReturnData.KTF = true;
+
+    return LocalReturnData;
+};
+
+let StartFunc_20Aug2024 = () => {
     let LocalForeignTables = LocalFuncReturnForeignTables();
 
     let LocalReturnData = { KTF: false }
@@ -42,6 +62,58 @@ let StartFunc = () => {
     });
 
     LocalReturnData.KTF = true;
+
+    return LocalReturnData;
+};
+
+let LocalFuncReturnFiles = () => {
+    let LocalFilesArray = [];
+    let LocalDataPath = `${ConfigJson.jsonConfig.DataPath}/${ConfigJson.jsonConfig.DataPk}`;
+
+    let filenames = fs.readdirSync(LocalDataPath);
+
+    filenames.forEach((file) => {
+        if (fs.lstatSync(`${LocalDataPath}/${file}`).isFile()) {
+            LocalFilesArray.push(file);
+        };
+    });
+
+    return LocalFilesArray;
+};
+
+let LocalFuncLoopFiles = ({ inForeignTables, inFileNames }) => {
+    let LocalForeignTables = inForeignTables;
+
+    let LocalReturnData = {};
+
+    let filenames = inFileNames;
+
+    filenames.forEach((file) => {
+        let LoopInsideTableName = path.parse(file).name;
+
+        if (LocalForeignTables.includes(LoopInsideTableName)) {
+            let LoopInsideTableColumns = ConfigJson.jsonConfig.tableAndColumns.children.find(element => {
+                return element.name === file;
+            });
+
+            let LoopInsideColumnName;
+
+            for (const [key, value] of Object.entries(LoopInsideTableColumns.fileData)) {
+                if (value.unique) {
+                    LoopInsideColumnName = key;
+                };
+            };
+
+            const data = fs.readFileSync(`${CommonDataPath}/${file}`, { encoding: 'utf8' });
+
+            let JsonParseData = JSON.parse(data);
+            let LoopInsideArrayNeeded = JsonParseData.map(element => {
+                return element[LoopInsideColumnName];
+            });
+
+            LocalReturnData[LoopInsideTableName] = LoopInsideArrayNeeded.sort();
+        };
+    });
 
     return LocalReturnData;
 };
