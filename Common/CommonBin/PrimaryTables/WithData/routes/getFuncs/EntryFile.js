50efff281edfc5d43d3fb09fc690f25d986f3f34
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2024-08-20 10:38:08 +0530

    AllUniques end point started

--- Common/CommonBin/PrimaryTables/WithData/routes/getFuncs/EntryFile.js
+++ Common/CommonBin/PrimaryTables/WithData/routes/getFuncs/EntryFile.js
@@ -4,5 +4,6 @@ var router = express.Router();
 import { GetFunc } from '../../controllers/getFuncs/EntryFile.js';
 
 router.get('/', GetFunc);
+router.get('/AllUniques', GetFunc);
 
 export { router };
