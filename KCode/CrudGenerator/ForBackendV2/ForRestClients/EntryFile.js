a4f7f157472959156cca9943d297095950966d70
keshavsoft <nknnkumar@yahoo.com>
2024-08-21 11:09:45 +0530

    put rest workings

--- KCode/CrudGenerator/ForBackendV2/ForRestClients/EntryFile.js
+++ KCode/CrudGenerator/ForBackendV2/ForRestClients/EntryFile.js
@@ -1,9 +1,11 @@
 import { StartFunc as Show } from './Show/EntryFile.js';
 import { StartFunc as Create } from './Create/EntryFile.js';
+import { StartFunc as Alter } from './Alter/EntryFile.js';
 
 let StartFunc = ({ inTablesCollection, inTo, inFrom }) => {
     Show({ inTablesCollection, inTo, inFrom });
     Create({ inTablesCollection, inTo, inFrom });
+    Alter({ inTablesCollection, inTo, inFrom });
 };
 
 export { StartFunc };
