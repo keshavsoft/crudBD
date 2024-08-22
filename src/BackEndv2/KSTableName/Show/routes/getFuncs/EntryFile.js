76eb31c6ebee3c3e59039aae682868ef3cad5540
keshavsoft <nknnkumar@yahoo.com>
2024-08-22 15:31:57 +0530

    v2 GetRowDataFunc started

--- src/BackEndv2/KSTableName/Show/routes/getFuncs/EntryFile.js
+++ src/BackEndv2/KSTableName/Show/routes/getFuncs/EntryFile.js
@@ -8,6 +8,7 @@ import {
 } from '../../controllers/getFuncs/EntryFile.js';
 
 router.get('/', GetFunc);
+// router.get('/:id', GetRowDataFunc);
 router.get('/DataOnly', GetDataOnlyFunc);
 router.get('/Images', GetImagesFunc);
 router.get('/BodyCheck', GetBodyCheckFunc);
@@ -15,6 +16,5 @@ router.get('/FromModal', GetFromModalFunc);
 router.get('/FromModalUuid', GetFromModalUuidFunc);
 router.get('/WithJoins', GetWithJoinsFunc);
 router.get('/Sort/:Column', GetDataSortByColumnFunc);
-
 
 export { router };
