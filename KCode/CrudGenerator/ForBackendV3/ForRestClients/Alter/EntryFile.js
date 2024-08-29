b675be447b3c6e019a6cd386469258b1cffe80a4
keshavsoft <nknnkumar@yahoo.com>
2024-08-29 17:36:20 +0530

    ALTER NEED TO TEST

--- KCode/CrudGenerator/ForBackendV3/ForRestClients/Alter/EntryFile.js
+++ KCode/CrudGenerator/ForBackendV3/ForRestClients/Alter/EntryFile.js
@@ -2,13 +2,14 @@ import fs from "fs";
 import path from "path";
 import dotenv from 'dotenv';
 dotenv.config();
+const CommonAlter = "Alter";
 
 let StartFunc = ({ inTablesCollection, inTo, inConfigJson }) => {
     LocalFuncForGetEndPoints({ inTablesCollection, inTo, inConfigJson });
 };
 
 let LocalFuncForGetEndPoints = ({ inTablesCollection, inTo, inConfigJson }) => {
-    let LocalTypeName = "Alter/restClients/PutEndPoints";
+    let LocalTypeName = `${CommonAlter}/restClients/PutEndPoints`;
     let LocalTo = inTo;
 
     let LocalTablesCollection = inTablesCollection;
@@ -21,21 +22,41 @@ let LocalFuncForGetEndPoints = ({ inTablesCollection, inTo, inConfigJson }) => {
         let LoopInsideFileName = path.parse(element.name).name;
         let LocalFilePath = `${LocalTo}/${LoopInsideFileName}/${LocalTypeName}`;
 
-        let LoopInsideFiles = fs.readdirSync(LocalFilePath, { withFileTypes: true })
-            .filter(item => !item.name.endsWith(".json"))
-            .map(item => item.name);
-
-        LoopInsideFiles.forEach(LoopFile => {
-            let LocalFileData = `PUT http://localhost:${process.env.PORT}/${LocalTo}/${LoopInsideFileName}/Alter\r\n\r\n`;
-
-            let LocalColumnsSchema = LocalFuncGetTableSchema({
-                inConfigJson,
-                inTableNameWithExtension: element.name
-            });
-
-            fs.writeFileSync(`${LocalFilePath}/${LoopFile}`, `${LocalFileData}\r\n${JSON.stringify(LocalColumnsSchema)}`);
+        LocalFuncWriteToHome({
+            inFrom: `${process.env.PORT}/${LocalTo}/${LoopInsideFileName}`,
+            inTo: `${LocalFilePath}`,
+            inTableNameWithExtension: element.name,
+            inConfigJson
         });
+
+
+        // let LoopInsideFiles = fs.readdirSync(LocalFilePath, { withFileTypes: true })
+        //     .filter(item => !item.name.endsWith(".json"))
+        //     .map(item => item.name);
+
+        // LoopInsideFiles.forEach(LoopFile => {
+        //     let LocalFileData = `POST http://localhost:${process.env.PORT}/${LocalTo}/${LoopInsideFileName}/Create\r\n\r\n`;
+
+        //     let LocalColumnsSchema = LocalFuncGetTableSchema({
+        //         inConfigJson,
+        //         inTableNameWithExtension: element.name
+        //     });
+
+        //     fs.writeFileSync(`${LocalFilePath}/${LoopFile}`, `${LocalFileData}\r\n${JSON.stringify(LocalColumnsSchema)}`);
+        // });
+
     });
+};
+
+const LocalFuncWriteToHome = ({ inFrom, inTo, inConfigJson, inTableNameWithExtension }) => {
+    let LocalFileData = `PUT http://localhost:${inFrom}/${CommonCreate}\r\nContent-Type: application/json\r\n`;
+
+    let LocalColumnsSchema = LocalFuncGetTableSchema({
+        inConfigJson,
+        inTableNameWithExtension
+    });
+
+    fs.writeFileSync(`${inTo}/home.http`, `${LocalFileData}\r\n${JSON.stringify(LocalColumnsSchema)}`);
 };
 
 const LocalFuncGetTableSchema = ({ inConfigJson, inTableNameWithExtension }) => {
