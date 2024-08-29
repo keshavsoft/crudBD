384d286d2a9bf487811764d573ddc48055b47eec
sharath0036 <ksharathbabu2@gmail.com>
2024-08-29 18:39:46 +0530

    writing new rest

--- src/BackEndv3/KSTableName/Show/dals/postFuncs/EntryFile.js
+++ src/BackEndv3/KSTableName/Show/dals/postFuncs/EntryFile.js
@@ -1,12 +1,7 @@
-import { StartFunc as ReadFromFile } from '../../kLowDb/ReadFromFile/getFunc.js';
-import { StartFunc as getModal } from '../../kLowDb/ReadFromFile/getModal.js';
-import { StartFunc as getBodyCheck } from '../../kLowDb/ReadFromFile/getBodyCheck.js';
-import { StartFunc as getimagesOnly } from '../../kLowDb/ReadFromFile/getimagesOnly.js';
-import { StartFunc as withJoins } from '../../kLowDb/ReadFromFile/withJoins.js';
-import { StartFunc as getRowFunc } from '../../kLowDb/ReadFromFile/getRowFunc.js';
+import { StartFunc as ApplyFilter } from '../../kLowDb/ReadFromFile/ApplyFilter/getFunc.js';
 
 let postFilterDataFromBodyFunc = ({ inFindKey, inFindValue }) => {
-    let LocalFromLowDb = ReadFromFile({ inFindKey, inFindValue });
+    let LocalFromLowDb = ApplyFilter({ inFindKey, inFindValue });
 
     if (LocalFromLowDb.KTF === false) {
         return false;
