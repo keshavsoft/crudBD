1b740eb828e6c2ca818d419764697c00d38cdaad
keshavsoft <nknnkumar@yahoo.com>
2024-08-29 13:01:27 +0530

    Laundry started in 318

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import { router as routerFromBinV2 } from "./binV2/routes.js";
 import { StartFunc as StartFuncPortListen } from "./PortListen.js";
 
 import { StartFunc as MiddleWaresBinSecured } from "./MiddleWares/MiddleWares.binSecured/EntryFile.js";
-import { StartFunc as StartFuncKWSServer } from "./Projects/KWSServer/EntryFile.js";
+// import { StartFunc as StartFuncKWSServer } from "./Projects/KWSServer/EntryFile.js";
 
 import express from 'express';
 import http from 'http';
@@ -49,7 +49,7 @@ app.use('/Common', routerFromCommon);
 app.use('/Custom', routerFromCustom);
 app.use('/Login', routerFromLogin);
 
-StartFuncKWSServer(server);
+// StartFuncKWSServer(server);
 
 function normalizePort(val) {
     var port = parseInt(val, 10);
