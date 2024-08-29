9d553963d24f6b6712c5b11362672989b3564e76
keshavsoft <nknnkumar@yahoo.com>
2024-08-29 16:39:06 +0530

    v3 rest show still need to write more

--- KCode/CrudGenerator/ForBackendV3/ForRestClients/Show/EntryFile.js
+++ KCode/CrudGenerator/ForBackendV3/ForRestClients/Show/EntryFile.js
@@ -20,17 +20,41 @@ let LocalFuncForGetEndPoints = ({ inTablesCollection, inTo }) => {
     LocalFirstLevelFolders.forEach(element => {
         let LoopInsideFileName = path.parse(element.name).name;
         let LocalFilePath = `${LocalTo}/${LoopInsideFileName}/${LocalTypeName}`;
-     
-        let LoopInsideFiles = fs.readdirSync(LocalFilePath, { withFileTypes: true })
-            .filter(item => !item.name.endsWith(".json"))
-            .map(item => item.name);
 
-        LoopInsideFiles.forEach(LoopFile => {
-            let LocalFileData = `GET http://localhost:${process.env.PORT}/${LocalTo}/${LoopInsideFileName}/Show/DataOnly`;
-            
-            fs.writeFileSync(`${LocalFilePath}/${LoopFile}`, LocalFileData);
+        LocalFuncWriteToHome({
+            inFrom: `${process.env.PORT}/${LocalTo}/${LoopInsideFileName}`,
+            inTo: `${LocalFilePath}`
         });
+
+        LocalFuncWriteToDataOnly({
+            inFrom: `${process.env.PORT}/${LocalTo}/${LoopInsideFileName}`,
+            inTo: `${LocalFilePath}`
+        });
+
+        // let LoopInsideFiles = fs.readdirSync(LocalFilePath, { withFileTypes: true })
+        //     .filter(item => !item.name.endsWith(".json"))
+        //     .map(item => item.name);
+
+        // LoopInsideFiles.forEach(LoopFile => {
+
+
+        //     // let LocalFileData = `GET http://localhost:${process.env.PORT}/${LocalTo}/${LoopInsideFileName}/Show/DataOnly`;
+
+        //     // fs.writeFileSync(`${LocalFilePath}/${LoopFile}`, LocalFileData);
+        // });
     });
+};
+
+const LocalFuncWriteToHome = ({ inFrom, inTo }) => {
+    let LocalFileData = `GET http://localhost:${inFrom}/Show`;
+
+    fs.writeFileSync(`${inTo}/home.http`, LocalFileData);
+};
+
+const LocalFuncWriteToDataOnly = ({ inFrom, inTo }) => {
+    let LocalFileData = `GET http://localhost:${inFrom}/DataOnly`;
+
+    fs.writeFileSync(`${inTo}/DataOnly.http`, LocalFileData);
 };
 
 export { StartFunc };
