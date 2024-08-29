4b480385f791de416012087521afaff5ca73fa37
keshavsoft <nknnkumar@yahoo.com>
2024-08-29 15:48:04 +0530

    v3 delete rest done

---
+++ KCode/CrudGenerator/ForBackendV3/ForRestClients/Delete/EntryFile.js
@@ -0,0 +1,56 @@
+import fs from "fs";
+import path from "path";
+import dotenv from 'dotenv';
+dotenv.config();
+
+let StartFunc = ({ inTablesCollection, inTo, inConfigJson }) => {
+    LocalFuncForGetEndPoints({ inTablesCollection, inTo, inConfigJson });
+};
+
+let LocalFuncForGetEndPoints = ({ inTablesCollection, inTo, inConfigJson }) => {
+    let LocalTypeName = "Delete/restClients/DeleteEndPoints";
+    let LocalTo = inTo;
+
+    let LocalTablesCollection = inTablesCollection;
+
+    let LocalFirstLevelFolders = LocalTablesCollection.children.filter(element => {
+        return "children" in element === false;
+    }).filter(element => element.name.endsWith(".json"))
+
+    LocalFirstLevelFolders.forEach(element => {
+        let LoopInsideFileName = path.parse(element.name).name;
+        let LocalFilePath = `${LocalTo}/${LoopInsideFileName}/${LocalTypeName}`;
+
+        let LoopInsideFiles = fs.readdirSync(LocalFilePath, { withFileTypes: true })
+            .filter(item => !item.name.endsWith(".json"))
+            .map(item => item.name);
+
+        LoopInsideFiles.forEach(LoopFile => {
+            let LocalFileData = `DELETE http://localhost:${process.env.PORT}/${LocalTo}/${LoopInsideFileName}/Delete\r\n\r\n`;
+
+            let LocalColumnsSchema = LocalFuncGetTableSchema({
+                inConfigJson,
+                inTableNameWithExtension: element.name
+            });
+
+            fs.writeFileSync(`${LocalFilePath}/${LoopFile}`, `${LocalFileData}\r\n${JSON.stringify(LocalColumnsSchema)}`);
+        });
+    });
+};
+
+const LocalFuncGetTableSchema = ({ inConfigJson, inTableNameWithExtension }) => {
+    let LocalChildren = inConfigJson.jsonConfig.tableAndColumns.children;
+    let LocalColumnsSchemaToReturn = {};
+
+    let LocalColumnsSchema = LocalChildren.find(element => {
+        return element.name === inTableNameWithExtension;
+    });
+
+    for (const [key, value] of Object.entries(LocalColumnsSchema.fileData)) {
+        LocalColumnsSchemaToReturn[key] = "";
+    };
+
+    return LocalColumnsSchemaToReturn;
+};
+
+export { StartFunc };
--- KCode/CrudGenerator/ForBackendV3/ForRestClients/EntryFile.js
+++ KCode/CrudGenerator/ForBackendV3/ForRestClients/EntryFile.js
@@ -1,11 +1,13 @@
 import { StartFunc as Show } from './Show/EntryFile.js';
 import { StartFunc as Create } from './Create/EntryFile.js';
 import { StartFunc as Alter } from './Alter/EntryFile.js';
+import { StartFunc as Delete } from './Delete/EntryFile.js';
 
 let StartFunc = ({ inTablesCollection, inTo, inFrom, inConfigJson }) => {
     Show({ inTablesCollection, inTo });
     Create({ inTablesCollection, inTo, inConfigJson });
     Alter({ inTablesCollection, inTo, inConfigJson });
+    Delete({ inTablesCollection, inTo, inConfigJson });
 };
 
 export { StartFunc };
