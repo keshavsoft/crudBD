eb2239b8dae1543ad242d03936e531227455e637
Sameerq7 <227r1a05q7@cmrtc.ac.in>
2024-09-02 18:34:25 +0530

    Custom All day end points

--- Custom/Clients/Laundry/Orders/routes.js
+++ Custom/Clients/Laundry/Orders/routes.js
@@ -3,7 +3,9 @@ import express from 'express';
 var router = express.Router();
 
 import { router as routerFromToday } from './Today/routes.js';
+import { router as routerFromAllDay } from './AllDay/routes.js';
 
 router.use('/Today', routerFromToday);
+router.use('/AllDay',routerFromAllDay);
 
 export { router };
