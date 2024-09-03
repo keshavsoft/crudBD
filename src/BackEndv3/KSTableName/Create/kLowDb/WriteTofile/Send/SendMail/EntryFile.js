dfc103d12ab30bb228b5c7c3c33dbca4ad648026
keshavsoft <nknnkumar@yahoo.com>
2024-09-03 15:24:35 +0530

    inDataInserted fixed

--- src/BackEndv3/KSTableName/Create/kLowDb/WriteTofile/Send/SendMail/EntryFile.js
+++ src/BackEndv3/KSTableName/Create/kLowDb/WriteTofile/Send/SendMail/EntryFile.js
@@ -1,8 +1,8 @@
 import { StartFunc as sendMailCC } from "./sendMailCC.js";
 
-let StartFunc = async ({  inDomainName, inDataInserted, inpk }) => {
-     if ("Email" in inDataToInsert) {
-        return await sendMailCC({ CCEmail: inDataToInsert.Email, inDomainName, inDataInserted, inpk })
+let StartFunc = async ({ inDomainName, inDataInserted, inpk }) => {
+    if ("Email" in inDataInserted) {
+        return await sendMailCC({ CCEmail: inDataInserted.Email, inDomainName, inDataInserted, inpk })
     }
 };
 
