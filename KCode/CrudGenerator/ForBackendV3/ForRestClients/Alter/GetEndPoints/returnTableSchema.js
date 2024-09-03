f370e10ae1a9f84ffaec299defc3af74a986696b
pushpadamalanka82 <pushpadamalanka@gmail.com>
2024-09-03 17:42:28 +0530

    endpoints and restclients for returnhtml

---
+++ KCode/CrudGenerator/ForBackendV3/ForRestClients/Alter/GetEndPoints/returnTableSchema.js
@@ -0,0 +1,26 @@
+const StartFunc = ({ inConfigJson, inTableNameWithExtension }) => {
+    let LocalChildren = inConfigJson.jsonConfig.tableAndColumns.children;
+    let LocalColumnsSchemaToReturn = {};
+
+    let LocalColumnsSchema = LocalChildren.find(element => {
+        return element.name === inTableNameWithExtension;
+    });
+
+    for (const [key, value] of Object.entries(LocalColumnsSchema.fileData)) {
+        switch (value.type) {
+            case "STRING":
+                LocalColumnsSchemaToReturn[key] = "";
+                break;
+            case "INTEGER":
+                LocalColumnsSchemaToReturn[key] = 0;
+                break;
+            default:
+                break;
+        };
+
+    };
+
+    return LocalColumnsSchemaToReturn;
+};
+
+export { StartFunc };
