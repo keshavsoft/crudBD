6d8f6439a0638bda7242dda9f8418b10c87e5d68
pushpadamalanka82 <pushpadamalanka@gmail.com>
2024-09-03 19:34:28 +0530

    middleware for alter

--- src/BackEndv3/KSTableName/Alter/routes/GetFuncs/EntryFile.js
+++ src/BackEndv3/KSTableName/Alter/routes/GetFuncs/EntryFile.js
@@ -5,8 +5,11 @@ var router = express.Router();
 import {
     GetFunc, GetReturnHtmlFunc
 } from '../../controllers/GetFuncs/EntryFile.js';
+import {
+    GetFunc as middlewaresGetFunc
+} from '../../middlewares/Alter/EntryFile.js';
 
-router.get('/:id/:inKey/:inValue', GetFunc);
+router.get('/:id/:inKey/:inValue', middlewaresGetFunc, GetFunc);
 router.get('/ReturnHtml/:id/:inKey/:inValue', GetReturnHtmlFunc);
 
 export { router };
