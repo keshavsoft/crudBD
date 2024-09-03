df31ff4cc6422a51ab73d9ffecb4a6ab46d63f87
Srivasu215 <sri.vasu215@gmail.com>
2024-09-03 14:36:06 +0530

    ASIS

--- Login/LoginBin/Users/controllers/postFuncs/EntryFile.js
+++ Login/LoginBin/Users/controllers/postFuncs/EntryFile.js
@@ -1,4 +1,9 @@
-import { PostFunc as PostFuncRepo, PostFuncWithMail as PostFuncRepoWithMail, PostFuncWithEndUser as PostFuncRepoWithEndUser, PostFuncWithDataPk as PostFuncRepoWithDataPk } from '../../repos/postFuncs/EntryFile.js';
+import {
+    PostFunc as PostFuncRepo, PostFuncWithMail as PostFuncRepoWithMail,
+    PostFuncWithEndUser as PostFuncRepoWithEndUser,
+    PostFuncWithDataPk as PostFuncRepoWithDataPk,
+    PostAsIsFunc as PostAsIsFuncRepo
+} from '../../repos/postFuncs/EntryFile.js';
 
 let PostFunc = async (req, res) => {
     let LocalData = req.body;
@@ -7,6 +12,17 @@ let PostFunc = async (req, res) => {
     let LocalPassword = LocalData.Password;
 
     let LocalFromRepo = await PostFuncRepo({ inUsername: LocalUsername, inPassword: LocalPassword });
+
+    if (LocalFromRepo.KTF === false) res.status(401).json(LocalFromRepo);
+
+    res.json(LocalFromRepo.JsonData);
+
+};
+
+let PostAsIsFunc = async (req, res) => {
+    let LocalData = req.body;
+
+    let LocalFromRepo = await PostAsIsFuncRepo({ inInsertData: LocalData });
 
     if (LocalFromRepo.KTF === false) res.status(401).json(LocalFromRepo);
 
@@ -60,4 +76,4 @@ let PostFuncWithDataPk = async (req, res) => {
     res.json(LocalFromRepo);
 }
 
-export { PostFunc, PostFuncWithMail, PostFuncEndUser, PostFuncWithDataPk };
+export { PostFunc, PostFuncWithMail, PostFuncEndUser, PostFuncWithDataPk, PostAsIsFunc };
