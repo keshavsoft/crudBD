399ed91dcecd2fd715a5dd44a8403139ddeba516
pushpadamalanka82 <pushpadamalanka@gmail.com>
2024-09-05 13:03:38 +0530

    new rests

---
+++ Custom/Clients/Laundry/Today/Qrcodes/InBranch/controllers/GetFuncs/EntryFile.js
@@ -0,0 +1,21 @@
+import {
+    GetFuncs as GetFuncsRepo
+} from '../../repos/GetFuncs/EntryFile.js';
+
+let GetFuncs = (req, res) => {
+    let LocalParams = req.params;
+    let LocalBranch = LocalParams.inBranch
+
+    let LocalFromRepo = GetFuncsRepo({ inBranch: LocalBranch });
+
+    if (LocalFromRepo.KTF === false) {
+        res.status(500).send(LocalFromRepo.KReason);
+        return;
+    };
+
+    res.status(200).json(LocalFromRepo);
+};
+
+export {
+    GetFuncs
+};
