3768bd22f31c7e2fc658597ab6940e6cf2982129
Srivasu215 <sri.vasu215@gmail.com>
2024-09-11 18:11:49 +0530

    CUSTOM ORDES

--- Custom/Clients/Laundry/Orders/routes.js
+++ Custom/Clients/Laundry/Orders/routes.js
@@ -2,12 +2,12 @@ import express from 'express';
 
 var router = express.Router();
 
-import { router as routerFromToday } from './Today/routes.js';
-import { router as routerFromAllDay } from './AllDay/routes.js';
+// import { router as routerFromToday } from './Today/routes.js';
+// import { router as routerFromAllDay } from './AllDay/routes.js';
 import { router as routerFromNewOrder } from './NewOrder/routes.js';
 
-router.use('/Today', routerFromToday);
-router.use('/AllDay',routerFromAllDay);
+// router.use('/Today', routerFromToday);
+// router.use('/AllDay',routerFromAllDay);
 router.use('/NewOrder',routerFromNewOrder);
 
 export { router };
