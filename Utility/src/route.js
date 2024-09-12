28916594cdca98ddfa8bdf78c3cedeca6945cb24
keshavsoft <nknnkumar@yahoo.com>
2024-09-12 16:06:39 +0530

    backup is secured

--- Utility/src/route.js
+++ Utility/src/route.js
@@ -2,9 +2,11 @@ import express from 'express';
 
 var router = express.Router();
 
+import { StartFunc as MiddleWaresBinSecured } from "../../MiddleWares/MiddleWares.binSecured/EntryFile.js";
+
 // import { router as routerpostFuncs } from './routes/postFuncs/EntryFile.js';
 import { router as routergetFuncs } from './routes/getFuncs/EntryFile.js';
 
-router.use('/backup', routergetFuncs);
+router.use('/backup', MiddleWaresBinSecured, routergetFuncs);
 
 export { router };
