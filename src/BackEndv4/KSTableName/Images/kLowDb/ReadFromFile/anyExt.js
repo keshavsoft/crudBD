15e8899089f4e6adee2c5b281c468a0b4c37c37d
keshavsoft <nknnkumar@yahoo.com>
2024-09-13 16:40:54 +0530

    v4 any extension working

---
+++ src/BackEndv4/KSTableName/Images/kLowDb/ReadFromFile/anyExt.js
@@ -0,0 +1,24 @@
+import Configjson from '../../../../Config.json' assert { type: 'json' };
+import tableNameJson from '../../../tableName.json' assert { type: 'json' };
+
+import path, { resolve } from 'path';
+
+let StartFunc = ({ inKey, inResponse }) => {
+  const LocalFilePath = `${inKey}.png`;
+
+  const localPath = path.parse(tableNameJson.tableName).name;
+  const LocalFolderPath = `${Configjson.jsonConfig.DataPath}/${Configjson.jsonConfig.DataPk}/${localPath}`;
+  let filenames = fs.readdirSync(LocalFolderPath);
+
+  let LocalFilteredArray = filenames.filter(file => {
+    return file.startsWith(inKey);
+  });
+
+  const userDataFilePath = `${Configjson.jsonConfig.DataPath}/${Configjson.jsonConfig.DataPk}/${localPath}/${LocalFilteredArray[0]}`;
+
+  const LocalImageAbsolutePath = resolve(__basedir, userDataFilePath);
+
+  inResponse.sendFile(LocalImageAbsolutePath);
+};
+
+export { StartFunc };
