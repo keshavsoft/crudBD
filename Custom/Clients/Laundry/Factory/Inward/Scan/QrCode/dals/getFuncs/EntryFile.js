bcbd1f77764275663aaa4592c708069310501c85
keshavsoft <nknnkumar@yahoo.com>
2024-09-18 16:33:44 +0530

    custom qrcodes working

--- Custom/Clients/Laundry/Factory/Inward/Scan/QrCode/dals/getFuncs/EntryFile.js
+++ Custom/Clients/Laundry/Factory/Inward/Scan/QrCode/dals/getFuncs/EntryFile.js
@@ -1,7 +1,7 @@
 // import { StartFunc as ApplyFilter } from '../../kLowDb/ReadFromFile/ApplyFilter/getFunc.js';
 import HomeJson from './home.json' with {type: 'json'};
 
-let GetFunc = ({ inFilterObject }) => {
+let GetFunc = () => {
     // let LocalFromLowDb = ApplyFilter({ inFilterObject });
 
     return HomeJson;
