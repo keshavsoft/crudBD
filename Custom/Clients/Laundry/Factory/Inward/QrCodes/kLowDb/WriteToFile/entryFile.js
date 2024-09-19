bd55aaa046227b6550a39557baee5c22272825e7
keshavsoft <nknnkumar@yahoo.com>
2024-09-19 10:38:24 +0530

    factory scan error

---
+++ Custom/Clients/Laundry/Factory/Inward/QrCodes/kLowDb/WriteToFile/entryFile.js
@@ -0,0 +1,31 @@
+import { StartFunc as StartFuncwriteFileFromModal } from './WithChecking/StartFunc.js';
+import { StartFuncForBookings as StartFuncCheckQrCodes } from "./Check/CheckQrCodes.js";
+import { StartFunc as CheckBrcnchScan } from "./Check/CheckBrcnchScan.js";
+
+let StartFunc = ({ inFactory, inDataInsert }) => {
+
+    let LocalTable = inFactory;
+    let LocalQrId = inDataInsert.QrCodeId;
+    let LocalDc = inDataInsert.VoucherRef;
+    let LocalDataInsert = inDataInsert;
+    let LocalReturnData = { KTF: false };
+
+    let LocalCheckQrCodes = StartFuncCheckQrCodes({ inTable: LocalTable, inQrId: LocalQrId });
+
+    if (LocalCheckQrCodes.KTF === false) {
+        LocalReturnData.KReason = LocalCheckQrCodes.KReason
+        return LocalReturnData;
+    };
+
+    let LocalCheckBrcnchScan = CheckBrcnchScan({ inTable: LocalTable, inDc: LocalDc, inQrCodeId: LocalQrId });
+
+    if (LocalCheckBrcnchScan.KTF === false) {
+        LocalReturnData.KReason = LocalCheckBrcnchScan.KReason
+        return LocalReturnData;
+    };
+
+    return StartFuncwriteFileFromModal({ inDataToInsert: LocalDataInsert });
+
+};
+
+export { StartFunc };
