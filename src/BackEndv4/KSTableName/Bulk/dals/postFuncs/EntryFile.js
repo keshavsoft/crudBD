1e11412e425c3c75c4718ea5301518f2907e47aa
keshavsoft <nknnkumar@yahoo.com>
2024-09-19 12:55:38 +0530

    bulk started

--- src/BackEndv4/KSTableName/Bulk/dals/postFuncs/EntryFile.js
+++ src/BackEndv4/KSTableName/Bulk/dals/postFuncs/EntryFile.js
@@ -1,17 +1,13 @@
 import { StartFunc as StartFuncWriteTofile } from '../../kLowDb/WriteTofile/WithChecking/StartFunc.js';
-import { StartFunc as SendMail } from '../../kLowDb/SendMail/EntryFile.js';
-import ConfigJson from '../../../../Config.json' assert {type: 'json'};
-import tableNameJson from '../../../tableName.json' assert {type: 'json'};
-
-import fs from "fs";
-import path from 'path';
 
 let PostFunc = (inPostBody) => {
     return StartFuncWriteTofile({ inDataToInsert: inPostBody });
 };
+
 let MultiInsertWithCheckFunc = (inPostBody) => {
     return StartFuncWriteTofile({ inDataToInsert: inPostBody });
 };
+
 export {
     PostFunc, MultiInsertWithCheckFunc
 };
